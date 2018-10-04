eac7cc5c8f3306c24c0bfcb37c75d7d65c5b1a7d
kevte89 <39636900+kevte89@users.noreply.github.com>
2018-10-04 16:14:12 +0200

    Update aicp.mk

--- aicp.mk
+++ aicp.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
-# Inherit some common Aicp stuff.
+# Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
